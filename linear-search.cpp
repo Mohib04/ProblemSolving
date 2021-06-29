15b8ab5156f46313250e917929adcb0398ee8b9b
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2021-06-29 09:30:43 +0600

    done

---
+++ linear-search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a[20];
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    ///Enter five elements for a[] arrary
+    cout<<"Enter the array elements: ";
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    ///Enter which element you want to find
+    int key;
+    cout<<"Enter the key which one you want to search: ";
+    cin>>key;
+    ///search the key into the array elements
+    int j;
+    for(j=0; j<n; j++){
+        if(key==a[j]){
+            cout<<key <<" Present at: "<<j<<" Position "<<endl;
+            break;
+        }
+    }
+
+    if(j==n){
+        cout<<key <<" Not present in the array!!" <<endl;
+    }
+
+
+    return 0;
+}
